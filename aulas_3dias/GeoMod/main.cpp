fe7bba83f34e17c2aabe031248c1c9d40fe81b5a
Paulo Carvalho <paulo.r.m.carvalho@gmail.com>
2019-05-03 14:17:11 -0300

    Fontes. WIP.

---
+++ aulas_3dias/GeoMod/main.cpp
@@ -0,0 +1,21 @@
+//#include <QApplication>
+
+//#include "gui/mainwindow.h"
+
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+    printf("Hello VTK!\n");
+    printf("Iniciando...\n");
+//    QApplication application(argc, argv);
+//    QApplication::setOrganizationName(APP_NAME);
+//    QApplication::setOrganizationDomain("geomod.unisinos.br");
+//    QApplication::setApplicationName(APP_NAME_VER);
+//    MainWindow mw;
+//    mw.show();
+
+//    return application.exec();
+    printf("Finalizou normalmente.\n");
+    return 0;
+}
